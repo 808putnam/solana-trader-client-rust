e4dc89ec129b621ab3e5b7210d03722c644b942f
808putnam <wmcroberts3@gmail.com>
2024-11-13 09:42:52 -0500

    chore: removing legacy custom serialization

--- src/provider/utils.rs
+++ src/provider/utils.rs
@@ -13,19 +13,6 @@ pub fn convert_string_enums(value: &mut Value) {
                         }
                     }
                     
-                    // String to numeric conversions
-                    // Leaving this commented out for now, 
-                    // as we try out a new custom deserialization in proto build
-                    /*
-                    (k, Value::String(s)) if ["tradeFeeRate", "height", "token1Reserves", 
-                                            "token2Reserves", "slot", "time", "openTime"]
-                                            .contains(&k) => {
-                        if let Ok(num) = s.parse::<i64>() {
-                            *val = json!(num);
-                        }
-                    }
-                    */
-                    
                     // Infinity enum conversion
                     ("infinity", Value::String(s)) => {
                         let mapped = match s.as_str() {
